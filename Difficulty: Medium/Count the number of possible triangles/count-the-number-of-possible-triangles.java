749d0a3c881bbece087a2bbeb981cdc7d07595b7
VivekxEverywhere <vivek.sharma_cs23@gla.ac.in>
2026-06-10 23:36:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countTriangles(int arr[]) {
+        int n = arr.length;
+        if (n < 3) return 0;
+        
+        java.util.Arrays.sort(arr);
+        int count = 0;
+        
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
+            
+            while (i < j) {
+                if (arr[i] + arr[j] > arr[k]) {
+                    count += (j - i);
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
